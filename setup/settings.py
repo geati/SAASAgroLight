1017a152b5cd160d19943e399c4e182ff77d51dd
Isabella Ramos <rmsisabella@gmail.com>
2024-12-15 12:16:57 -0300

    Conectando o banco de dados do projeto

--- setup/settings.py
+++ setup/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Saas_agro_light',
+        'USER': 'postgres',
+        'PASSWORD': '123654',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
