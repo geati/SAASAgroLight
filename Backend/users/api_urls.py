82b07dceee347c21009a31ad7b34e44aec4c8405
Isabella Ramos <rmsisabella@gmail.com>
2025-10-05 11:38:58 -0300

    Ajuste usuários e tabela auth_user

--- Backend/users/api_urls.py
+++ Backend/users/api_urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
-from .views import solicitar_redefinicao, redefinir_senha
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UsuarioViewSet, solicitar_redefinicao, redefinir_senha
+
+router = DefaultRouter()
+router.register(r'usuarios', UsuarioViewSet, basename='usuario')
 
 urlpatterns = [
+    # rotas customizadas
     path('solicitar-redefinicao/', solicitar_redefinicao, name='solicitar_redefinicao'),
-    path('redefinir-senha/<uidb64>/<token>/', redefinir_senha, name='redefinir_senha'),
+    path('redefinir-senha/<str:uidb64>/<str:token>/', redefinir_senha, name='redefinir_senha'),
+    
+    # rotas do router
+    path('', include(router.urls)),
 ]
